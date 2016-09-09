c71edcf91766780fcb56ba7a93b4002ca08fbaaf
zat00 <zat00@DESKTOP-1KOU5ED>
2016-09-09 16:08:19 -0500

    Initial Commit

---
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -0,0 +1,25 @@
+class Angle():
+    def __init__(self):
+        self.angle = 0.0       #set to 0 degrees 0 minutes
+        pass
+    
+    def setDegrees(self, degrees):
+        pass
+    
+    def setDegreesAndMinutes(self, degrees):
+        pass
+    
+    def add(self, angle):
+        pass
+    
+    def subtract(self, angle):
+        pass
+    
+    def compare(self, angle):
+        pass
+    
+    def getString(self):
+        pass
+    
+    def getDegrees(self):
+        pass
