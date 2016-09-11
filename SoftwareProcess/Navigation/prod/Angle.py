832b31111711f68b53945d014d6103771d61fb74
zat00 <zat00@DESKTOP-1KOU5ED>
2016-09-11 00:11:01 -0500

    __init__
    Signed-off-by: zat00 <zat00@DESKTOP-1KOU5ED>

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,19 +1,39 @@
 class Angle():
     def __init__(self):
-        self.angle = 0.0       #set to 0 degrees 0 minutes
-        pass
+        #set to 0 degrees 0 minutes
+        self.degrees = 0
+        self.minutes = 0.0
     
-    def setDegrees(self, degrees):
-        pass
+    def setDegrees(self, degrees=None):
+        if (degrees == None):
+            self.degrees = 0
+        else:
+            self.degrees = degrees%360
+        return float(self.degrees)
     
-    def setDegreesAndMinutes(self, degrees):
-        pass
+    def setDegreesAndMinutes(self, angleString):
+        delimeter = 'd'
+        delimiterIndex = angleString.find(delimeter)
+        self.degrees = int(angleString[:delimiterIndex])
+        self.minutes = float(angleString[delimiterIndex+1:])
+        self.degrees = self.degrees%360
+        self.degrees = self.degrees + (int(self.minutes / 60))
+        self.minutes = (self.minutes%60)
+        output = float(self.degrees + (self.minutes / 100))
+        return output
     
     def add(self, angle):
-        pass
+        self.degrees = (self.degrees + angle.degrees)%360
+        self.degrees = self.degrees + (int((self.minutes + angle.minutes) / 60))
+        self.minutes = (self.minutes + angle.minutes)%60.0
+        output = float(self.degrees + (self.minutes / 100))
+        return output
     
     def subtract(self, angle):
-        pass
+        self.degrees = (self.degrees - angle.degrees)%360
+        self.minutes = ((self.minutes - angle.minutes)%60.0)
+        output = float(self.degrees + self.minutes)
+        return output
     
     def compare(self, angle):
         pass
@@ -22,4 +42,13 @@ class Angle():
         pass
     
     def getDegrees(self):
-        pass
+        return self.degrees
+    
+myAngle = Angle()
+myAngle2 = Angle()
+angleString = '10d179'
+angleString2 = '3d1'
+myAngleFloat = myAngle.setDegreesAndMinutes(angleString)
+myAngleFloat2 = myAngle2.setDegreesAndMinutes(angleString2)
+myAngleFloat = myAngle.add(myAngle2)
+print(myAngleFloat)
