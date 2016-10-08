fa13af552dcfb34035fbaad0dd3b41a292b16e60
zat00 <zat00@DESKTOP-1KOU5ED>
2016-10-08 18:38:15 -0500

    Insure that setSigtingFile includes an argument
    Signed-off-by: zat00 <zat00@DESKTOP-1KOU5ED>

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -10,4 +10,19 @@ class Fix():
         '''
         Constructor
         '''
+        if (logFile == None):
+            self.fileName = "log.txt"
+        else:
+            self.fileName = logFile
         pass
+    
+        if (len(self.fileName) < 1):
+            raise ValueError('Fix.__init__:  Filename must be at least 1 character long')
+    
+    def setSightingFile(self, sightingFile=None):
+        if(sightingFile == None):
+            raise ValueError('Fix.setSightingFile:  expected a sightingFile')
+        return True
+    
+    
+        
--- SoftwareProcess/Navigation/test/FixTest.py
+++ SoftwareProcess/Navigation/test/FixTest.py
@@ -7,17 +7,23 @@ import Navigation.prod.Fix as Fix
 
 class FixTest(unittest.TestCase):
 
-    def setup(self):
-        pass
-    
+    def setUp(self):
+        self.className = "Fix."   
     def tearDown(self):
         pass
     
     def testName(self):
         pass
 
-    def test0InitializationTest(self):
+    def test000InitializationTest(self):
         self.assertIsInstance(Fix.Fix(), Fix.Fix)
+        
+    def test005setSightingFileValueError(self):
+        aFix = Fix.Fix()
+        expectedDiag = self.className + "setSightingFile:"
+        with self.assertRaises(ValueError) as context:
+            aFix.setSightingFile()
+        self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)])
         
 
 
