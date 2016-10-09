941a11678420c9ee7b718f83b23892cc5cceaeac
zat00 <zat00@DESKTOP-1KOU5ED>
2016-10-09 18:49:28 -0500

    File checking in setSightingFile/changed structure of sightingFiles
    Signed-off-by: zat00 <zat00@DESKTOP-1KOU5ED>

--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -14,29 +14,48 @@ class Fix():
         '''
         if (logFile == None):
             self.fileName = "log.txt"
-            self.sightingFile = ""
+            self.sightingFiles = [None]
         else:
             self.fileName = logFile
-            self.sightingFile = ""
+            self.sightingFiles = [None]
     
         if (len(self.fileName) < 1):
             raise ValueError('Fix.__init__:  Filename must be at least 1 character long')
         
         if (os.path.isfile('./' + self.fileName)):
             self.sightings = open(self.fileName, 'a')
-            print('append')
         else:
             self.sightings = open(self.fileName, 'w')
-            print('write')
+            
+        self.fileIterator = 0
     
     def setSightingFile(self, sightingFile=None):
         if(sightingFile == None):
             raise ValueError('Fix.setSightingFile:  expected a sightingFile')
         elif((sightingFile[(len(sightingFile)-4):]) != ".xml"):
             raise ValueError('Fix.setSightingFile: sightingFile must be an xml file')
-        if(sightingFile == self.sightingFile):
-            return False
-        self.sightingFile = sightingFile
+        for i in range(len(self.sightingFiles)):
+            if(sightingFile == self.sightingFiles[i]):
+                return False
+        
+        if (os.path.isfile('./' + sightingFile)):
+            try:
+                tryToOpen = open(sightingFile, 'a')
+                tryToOpen.close()
+            except:
+                raise ValueError('Fix.setSightingFile: unable to open sightingFile')
+            self.sightingFiles[self.fileIterator] = sightingFile
+            self.sightingFiles.append(None)
+            self.fileIterator = self.fileIterator + 1
+        else:
+            try:
+                tryToOpen = open(sightingFile, 'w')
+                tryToOpen.close()
+            except:
+                raise ValueError('Fix.setSightingFile: unable to create sightingFile')
+            self.sightingFiles[self.fileIterator] = sightingFile
+            self.sightingFiles.append(None)
+            self.fileIterator = self.fileIterator + 1
         return True
     
     def getSightings(self):
