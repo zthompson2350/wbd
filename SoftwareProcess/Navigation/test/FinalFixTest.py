eebfa3c5fecc4bf87f1682a0ef0006abaeaeaaf7
zat00 <zat00@DESKTOP-1KOU5ED>
2016-12-03 15:08:37 -0600

    Added more initialization test code
    Signed-off-by: zat00 <zat00@DESKTOP-1KOU5ED>

--- SoftwareProcess/Navigation/test/FinalFixTest.py
+++ SoftwareProcess/Navigation/test/FinalFixTest.py
@@ -29,6 +29,72 @@ class TestFix2(unittest.TestCase):
         'Fix.__init__'
         self.assertIsInstance(F.Fix(), F.Fix, 
                               "Major error:  Fix not created")
+        
+    def test100_020_ShouldConstructFixWithDefaultFile(self):
+        theFix = F.Fix()
+        try:
+            theLogFile = open(self.DEFAULT_LOG_FILE, 'r')
+            entry = theLogFile.readline()
+            del theLogFile
+            self.assertNotEquals(-1, entry.find("Log file"), 
+                                 "Minor:  first line of log is incorrect")
+        except IOError:
+            self.fail()
+        self.assertIsInstance(theFix, F.Fix, 
+                              "Major:  log file failed to create")
+        
+    def test100_025_ShouldConstructWithKeywordParm(self):
+        try:
+            theFix = F.Fix(logFile=self.RANDOM_LOG_FILE)
+            self.assertTrue(True)
+        except:
+            self.fail("Minor: incorrect keyword specified")
+            self.cleanup()
+            
+    def test100_030_ShouldConstructFixWithNamedFile(self):
+        theFix = F.Fix(self.RANDOM_LOG_FILE)
+        try:
+            theLogFile = open(self.RANDOM_LOG_FILE, 'r')
+            entry = theLogFile.readline()
+            del theLogFile
+            self.assertNotEquals(-1, entry.find(self.logStartString), 
+                                 "Minor:  first line of log is incorrect")
+        except IOError:
+            self.fail()
+        self.assertIsInstance(theFix, F.Fix, "major:  log file failed to create")
+        self.cleanup() 
+        
+    def test100_040_ShouldConstructFixWithExistingFile(self):
+        theFix = F.Fix(self.RANDOM_LOG_FILE)
+        theFix = F.Fix(self.RANDOM_LOG_FILE)
+        try:
+            theLogFile = open(self.RANDOM_LOG_FILE, 'r')
+            numberOfExpectedEntries = 2
+            for _ in range(numberOfExpectedEntries):
+                entry = theLogFile.readline()
+                self.assertNotEquals(-1, entry.find(self.logStartString), 
+                                     "Minor:  first line of log is incorrect")
+        except IOError:
+            self.fail()
+        self.assertIsInstance(theFix, F.Fix, 
+                              "Major:  log file failed to create")
+        pass
+        del theLogFile
+        self.cleanup()
+        
+    def test100_910_ShouldRaiseExceptionOnFileNameLength(self):
+        expectedDiag = self.className + "__init__:"
+        with self.assertRaises(ValueError) as context:
+            F.Fix("")
+        self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)], 
+                          "Minor:  failure to check for log file name length") 
+        
+    def test100_920_ShouldRaiseExceptionOnNonStringFile(self):
+        expectedDiag = self.className + "__init__:"
+        with self.assertRaises(ValueError) as context:
+            F.Fix(42)
+        self.assertEquals(expectedDiag, context.exception.args[0][0:len(expectedDiag)], 
+                          "Minor:  failure to check for non-string log file name") 
 
 
 #  helper methods
